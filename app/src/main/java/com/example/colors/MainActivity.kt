52f3f666f98250fe7d73afa801a0bb853c314703
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-04-20 13:59:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/colors/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.colors
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val CREATE_BUTTON = findViewById<Button>(R.id.Create_button)
+        CREATE_BUTTON.setOnClickListener{combineInputs()}
+         }
+
+ private fun combineInputs() {
+        val BLUE_CHANNEL = findViewById<TextInputLayout>(R.id.Blue_Channel)
+        val RED_CHANNEL = findViewById<TextInputLayout>(R.id.Red_Channel)
+        val GREEN_CHANNEL = findViewById<TextInputLayout>(R.id.Green_Channel)
+
+
+        val BLUE = BLUE_CHANNEL.editText?.text.toString()
+        val RED = RED_CHANNEL.editText?.text.toString()
+        val GREEN = GREEN_CHANNEL.editText?.text.toString()
+
+        if(BLUE.isEmpty() or GREEN.isEmpty() or BLUE.isEmpty()) {
+            Toast.makeText(this, "All Values are required", Toast.LENGTH_LONG).show()
+
+        }
+     else {
+
+            var completeColorCode = "#$RED$GREEN$BLUE"
+            val COLOR_DISPLAY = findViewById<TextView>(R.id.color_display)
+            COLOR_DISPLAY.setBackgroundColor(Color.parseColor(completeColorCode))
+
+        }
+    }
+
+}
